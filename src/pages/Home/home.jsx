351526cf53ad3d0f16f0de817a769d66f16e9dfa
Jonizz14 <jahongirtuhtaev989@gmail.com>
2025-09-25 23:45:53 +0500

    jgsvajdg

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './home.css'
+import { FiBookOpen, FiUsers } from "react-icons/fi";
+import { LuGraduationCap } from "react-icons/lu";
 
 function home() {
   return (
-    <div>home</div>
+    <div className="welcome-container">
+      <div className="welcome-overlay">
+        <div className="welcome-content">
+          <h1 className="welcome-title">Bizning maktabimizga xush kelibsiz</h1>
+          <p className="welcome-subtitle">Kelajak uchun ta'lim</p>
+
+          <div className="features-section">
+            <button className="feature-card-primary-card">
+              <FiBookOpen size={20} />
+              <span>Oxirgi yangiliklar</span>
+            </button>
+
+            <button className="feature-card-blur-card">
+              <FiUsers size={20} />
+              <span>Bizning ustozlar</span>
+            </button>
+          </div>
+        </div>
+      </div>  
+    </div>
+    
   )
 }
 

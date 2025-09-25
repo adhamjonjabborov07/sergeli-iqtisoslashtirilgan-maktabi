1cd4e75c82154c4a1a8f1dac230371ba29635c30
adhamjon0770 <isagi3572@gmail.com>
2025-09-25 17:23:00 +0500

    dson

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,23 +1,59 @@
-import React from 'react'
-import "./footer.css"
-import { FaInstagram } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./footer.css";
+import { FaInstagram, FaYoutube, FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import Logo from "../Footer/logo.svg";
-function footer() {
+
+function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-
+        <div className="footer-col">
+          <Link to={"/"}>
+            <div className="header-logo-div">
+              <img className="logo-footer" src={Logo} alt="" />
+              <div>
+                <p>Bizning Maktab</p>
+                <p>Kelajak uchun ta’lim</p>
+              </div>
+            </div>
+          </Link>
+          <div className="footer-map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d551.5804135650701!2d69.2218422139766!3d41.210502803385936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae61c403191f6d%3A0xc8199e959d144b2e!2sSergeli%20ixtisoslashtirilgan%20maktab!5e0!3m2!1sru!2s!4v1758799393697!5m2!1sru!2s"
+              width="100%"
+              height="250"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Google Map"
+            ></iframe>
+          </div>
+        </div>
 
         <div className="footer-col">
-          <img src={Logo} alt="Company Logo" className="footer-logo" />
-          <p className="footer-address">
-            5123 Market St. #22B <br />
-            Charlotttesville, California <br />
-            44635
-          </p>
+          <h3>Quick Links</h3>
+          <Link to="/" className="footer-link">
+            Asosiy
+          </Link>
+          <Link to="/news" className="footer-link">
+            Yangiliklar
+          </Link>
+          <Link to="/teachers" className="footer-link">
+            O'qituvchilar
+          </Link>
+          <Link to="/schedule" className="footer-link">
+            Dars Jadvali
+          </Link>
+          <Link to="/announcements" className="footer-link">
+            E'lonlar
+          </Link>
+          <Link to={"/about"}>Biz Haqimizda</Link>
+        </div>
+
+        <div className="footer-col">
+          <h3>Bog'lanish uchun</h3>
           <a href="tel:+998901234567" className="footer-link">
             +998 90 123 45 67
           </a>
@@ -26,39 +62,39 @@ function footer() {
           </a>
         </div>
 
-
-        <div className="footer-col">
-          <h3>Quick Links</h3>
-          <Link to="/products" className="footer-link">Product</Link>
-          <Link to="/info" className="footer-link">Information</Link>
-          <Link to="/company" className="footer-link">Company</Link>
-          <Link to="/about" className="footer-link">About Us</Link>
-        </div>
-
-
-        <div className="footer-col">
-          <h3>Policies</h3>
-          <Link to="/privacy" className="footer-link">Privacy Policy</Link>
-          <Link to="/terms" className="footer-link">Terms & Conditions</Link>
-        </div>
-
-  
         <div className="footer-col">
           <h3>Follow Us</h3>
           <div className="footer-socials">
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube />
+            </a>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
           </div>
         </div>
-
       </div>
 
       <div className="footer-bottom">
         <p>© 2025 Lift Media. All rights reserved.</p>
       </div>
     </footer>
-  )
+  );
 }
 
-export default footer
+export default Footer;

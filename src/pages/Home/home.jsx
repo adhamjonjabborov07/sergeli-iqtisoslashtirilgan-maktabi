5523c8ced273b3a259c715110c77a3ca0c9cd269
Jonizz14 <jahongirtuhtaev989@gmail.com>
2025-09-26 14:46:36 +0500

    adfsdfs

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./home.css";
 import { FiBookOpen, FiUsers } from "react-icons/fi";
 import { LuGraduationCap } from "react-icons/lu";
-import { CiStar } from "react-icons/ci";
+import { GiTrophyCup } from "react-icons/gi";
+import { FaRegHeart } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import { Link } from "react-router-dom";
@@ -56,7 +57,7 @@ function Home() {
         <div className="section-div">
           <div className="section-div-card">
             <p className="section-div-card-icon">
-              <LuGraduationCap />
+              <LuGraduationCap size={30} />
             </p>
             <p className="section-div-card-p1">
 
@@ -67,7 +68,7 @@ function Home() {
           </div>
           <div className="section-div-card">
             <p className="section-div-card-icon">
-              <LuGraduationCap />
+              <FiUsers size={30} />
             </p>
             <p className="section-div-card-p1">
 
@@ -78,7 +79,7 @@ function Home() {
           </div>
           <div className="section-div-card">
             <p className="section-div-card-icon">
-              <LuGraduationCap />
+              <FiBookOpen size={30} />
             </p>
             <p className="section-div-card-p1">
 
@@ -86,6 +87,58 @@ function Home() {
             <p className="section-div-card-p2">
               Sinflar
             </p>
+          </div>
+        </div>
+      </section>
+      <section className="advantages-block">
+        <div className="advantages-wrapper">
+          <div className="advantages-heading">
+            <h2 className="advantages-main-title">Nega bizni tanlashadi?</h2>
+            <p className="advantages-intro-text">
+              Biz zamonaviy metodika va texnologiyalardan foydalangan holda sifatli ta'lim taqdim etamiz
+            </p>
+          </div>
+
+          <div className="benefits-container">
+            <div className="benefit-card">
+              <p className="benefit-card-icon">
+                <FiUsers size={30} />
+              </p>
+              <h3 className="benefit-name">Tajribali o'qituvchilar</h3>
+              <p className="benefit-info">
+                Ko'p yillik tajribaga ega professional pedagoglar
+              </p>
+            </div>
+
+            <div className="benefit-card">
+              <p className="benefit-card-icon">
+                <FiBookOpen size={30} />
+              </p>
+              <h3 className="benefit-name">Zamonaviy dasturlar</h3>
+              <p className="benefit-info">
+                Dolzarb ta'lim dasturlari va metodikalari
+              </p>
+            </div>
+
+            <div className="benefit-card">
+              <p className="benefit-card-icon">
+                <GiTrophyCup size={30} />
+              </p>
+              <h3 className="benefit-name">Yuqori natijalar</h3>
+              <p className="benefit-info">
+                O'quvchilarimizning a'lo darajadagi akademik yutuqlari
+              </p>
+            </div>
+
+            <div className="benefit-card">
+              <p className="benefit-card-icon">
+                <FaRegHeart size={30} />
+              </p>
+              <h3 className="benefit-name">Individual yondashuv</h3>
+              <p className="benefit-info">
+                Har bir o'quvchiga alohida e'tibor va g'amxo'rlik
+              </p>
+            </div>
           </div>
         </div>
       </section>

3682e17946539c2d0a97f5bd069f0d4637f8401d
adhamjon0770 <isagi3572@gmail.com>
2025-09-26 14:51:56 +0500

    fsefd

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -13,22 +13,41 @@ function Footer() {
             <div className="footer-logo-div">
               <img className="logo-footer" src={Logo} alt="" />
               <div>
-                <p>Bizning Maktab</p>
-                <p>Kelajak uchun ta’lim</p>
+                <p className="footer-logo-text">Bizning Maktab</p>
+                <p className="footer-logo-text">Kelajak uchun ta’lim</p>
               </div>
             </div>
           </Link>
-          <div className="footer-map">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d551.5804135650701!2d69.2218422139766!3d41.210502803385936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae61c403191f6d%3A0xc8199e959d144b2e!2sSergeli%20ixtisoslashtirilgan%20maktab!5e0!3m2!1sru!2s!4v1758799393697!5m2!1sru!2s"
-              width="100%"
-              height="250"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Google Map"
-            ></iframe>
+            <div className="footer-col">
+            <a href="tel:+998901234567" className="footer-link">
+              +998 90 123 45 67
+            </a>
+            <a href="mailto:info@liftmedia.com" className="footer-link">
+              info@liftmedia.com
+            </a>
+          </div>
+          <div className="footer-socials">
+            <a
+              href="https://www.instagram.com/sergeli_ixtisos_maktabi/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTelegram />
+            </a>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
           </div>
         </div>
         <div className="footer-flex">
@@ -52,40 +71,21 @@ function Footer() {
             <Link to={"/about"}>Biz Haqimizda</Link>
           </div>
 
-          <div className="footer-col">
-            <h3>Bog'lanish uchun</h3>
-            <a href="tel:+998901234567" className="footer-link">
-              +998 90 123 45 67
-            </a>
-            <a href="mailto:info@liftmedia.com" className="footer-link">
-              info@liftmedia.com
-            </a>
-          </div>
+        
         </div>
         <div className="footer-col">
-          <h3>Bizga obuna bo'ling</h3>
-          <div className="footer-socials">
-            <a
-              href="https://www.instagram.com/sergeli_ixtisos_maktabi/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTelegram />
-            </a>
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaFacebook />
-            </a>
+          <h3>Bizning manzilimiz</h3>
+           <div className="footer-map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d551.5804135650701!2d69.2218422139766!3d41.210502803385936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae61c403191f6d%3A0xc8199e959d144b2e!2sSergeli%20ixtisoslashtirilgan%20maktab!5e0!3m2!1sru!2s!4v1758799393697!5m2!1sru!2s"
+              width="100%"
+              height="250"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Google Map"
+            ></iframe>
           </div>
         </div>
 
